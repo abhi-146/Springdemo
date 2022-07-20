29e60d7ebe183fe4df64c7558a851c6768e176f9
Abhinav Jain <58175317+abhi-146@users.noreply.github.com>
2022-07-20 20:05:58 +0530

    Add Files

---
+++ Springdemo/src/main/java/com/springdemo/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.springdemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping
+    public String helloWorld(){
+        return "Hello World from Spring Boot";
+    }
+    @RequestMapping("/goodbye")
+    public String goodbye() {
+        return "Goodbye from Spring Boot";
+    }
+
+}
